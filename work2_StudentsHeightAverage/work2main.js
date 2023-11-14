c0882b75273143a6f590843212967eaa299bd9ee
Agary921127 <garryy1127@gmail.com>
2023-11-14 21:48:36 +0800

    work2 done but i think i can do batter

--- work2_StudentsHeightAverage/work2main.js
+++ work2_StudentsHeightAverage/work2main.js
@@ -7,24 +7,52 @@ d3.csv('SHA.csv').then(
 function drawStudentsHeightAverage(res){
     console.log(res);
     let myGraphW3 = document.getElementById('myGraphW2');
-
-    let traceW3_1 = {};
-    traceW3_1.mode = "markers+lines";
-    traceW3_1.type ="scatter";
-    traceW3_1.x = []
-    traceW3_1.y = []
-
+    let traceW3_male = {};
+    traceW3_male.mode = "markers";
+    traceW3_male.type ="scatter";
+    traceW3_male.name = "男生";
+    traceW3_male.x = []
+    traceW3_male.y = []
     for (let i=0;i<res.length;i++){
-        traceW3_1.x[i] = res[i]["學年度"]
-        traceW3_1.y[i] = res[i]["總計"]
+        traceW3_male.x[i] = res[i]["年齡"]
+        traceW3_male.y[i] = res[i]["男"]
+    }
+    let traceW3_female = {};
+    traceW3_female.mode = "markers";
+    traceW3_female.type ="scatter";
+    traceW3_female.name = "女生";
+    traceW3_female.x = []
+    traceW3_female.y = []
+    for (let i=0;i<res.length;i++){
+        traceW3_female.x[i] = res[i]["年齡"]
+        traceW3_female.y[i] = res[i]["女"]
     }
     let data =[];
-    data.push(traceW3_1);
-
+    data.push(traceW3_male);
+    data.push(traceW3_female);
     let layout ={
         margin:{
             t:50
         },
+        title: {
+            text: '台灣男女身高年齡關係圖',
+            font: {
+                family: 'Arial',      
+                size: 20,             
+                color: 'black'        
+            }
+        },
+        xaxis:{
+            title: "Age"
+        },
+        yaxis:{
+            title:{
+                text: "Height",
+                standoff: 9,
+                angle: 0
+            },
+            automargin: true,
+        }
     };
     Plotly.newPlot(myGraphW3, data, layout);
 }
